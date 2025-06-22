ff5d68d0e5fbbbdc961b9876ec90343bce0217c9
Hoang Anh <ngphuchoanganh@gmail.com>
2025-06-22 23:20:54 +0700

    feat: tailwind-variants
    - Use bun pm
    - Replace tv with cva since cva is not as feature rich as the former

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,8 +5,9 @@ export default function Counter() {
 	const [count, setCount] = createSignal(0);
 	return (
 		<Button
-			class="w-[200px] rounded-full border-2 px-[2rem] py-[1rem]"
-			onClick={() => setCount(count() + 1)}
+			variant="secondary"
+			size="lg"
+			onClick={() => setCount((prev) => prev + 1)}
 		>
 			Clicks: {count()}
 		</Button>
